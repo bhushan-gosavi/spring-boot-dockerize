24b7b307c63ed03c58ac54f1e9d38e3e7084cb06
bgosavi <bgosavi@qualys.com>
2021-12-14 14:41:20 +0530

    building war file instead of jar file

--- src/main/java/com/javatechie/spring/boot/docker/demo/SpringBootDockerApplication.java
+++ src/main/java/com/javatechie/spring/boot/docker/demo/SpringBootDockerApplication.java
@@ -1,17 +1,23 @@
 package com.javatechie.spring.boot.docker.demo;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+
 @SpringBootApplication
 @RestController
 public class SpringBootDockerApplication {
 
+	private static Logger logger = LogManager.getLogger(SpringBootDockerApplication.class);
 	@GetMapping("/message")
 	public String getMessage() {
-		return "Welcome to JavaTechie..!!";
+		logger.info("Printing log using log4j!");
+		return "Welcome to JavaApp..!!";
 	}
 
 	public static void main(String[] args) {
